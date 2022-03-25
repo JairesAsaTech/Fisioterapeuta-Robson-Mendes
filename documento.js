3caf34df1e768a9b352b05c221204309bdb0fd46
JairesAsaTech <102371550+JairesAsaTech@users.noreply.github.com>
2022-03-25 11:41:09 -0300

    mod1

---
+++ documento.js
@@ -0,0 +1,10 @@
+function abrirmenu(){
+    document.getElementById('abrir').style.marginLeft = '-880vh';
+   }
+   /*   quando clicar no icone menu vai aparecer a janela do menu na tela   */
+   
+   function fecharmenu(){
+       document.getElementById('abrir').style.marginLeft = '0vh';   
+    }
+
+   /*   quando clicar em fechar, a janela do menu vai desaparecer  */
